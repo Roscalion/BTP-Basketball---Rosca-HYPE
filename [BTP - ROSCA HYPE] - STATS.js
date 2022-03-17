5a9f2485346b3d7cdfe63a32ac989434fffa8968
Roscalion <Roscalion@users.noreply.github.com>
2022-03-16 19:52:27 -0600

    Rosca HYPE Javascript
    Javascript file of BTP Rosca HYPE

---
+++ [BTP - ROSCA HYPE] - STATS.js
@@ -0,0 +1,128 @@
+// BTP - ROSCA HYPE STATS //
+
+// Goal: To have my BTP player's stats fully played correctly and in Javascript Code. 
+
+
+
+
+// PLAYER INFOMRATION //
+
+const playerInformation = {
+
+    playerName: "Rosca HYPE",  
+    position: "pointGuard",
+    tendencies: "stephenCurry",
+
+}
+
+
+
+
+
+// PRIMARY SKILL - Shooting  - Specializes in 2pt/3pt shots -- Can be upgraded to HOF//
+
+const shootingStats = {
+    closeShot: 70,
+    midRangeShot: 92,
+    threePointShot: 92,
+    freeThrow: 80,
+    shotIQ: 92,
+    offensiveConsistency: 92,
+}
+
+
+// SECONDARY SKILL - Playmaking - Specializes in ball passing and assists -- Can be upgraded to Gold//
+
+
+const playmakingStats = {
+    passAccuracy: 86,
+    ballHandle: 86,
+    passIQ: 70,
+    passVision: 86,
+    speedWithBall: 95,
+
+}
+
+
+// SECONDARY SKILL 2 - Defense - Specailzies in defending the ball, and steals -- Can be upgraded to Silver//
+
+
+const defenseStats = {
+    interiorDefense: 60,
+    perimeterDefense: 86,
+    steal: 70, 
+    block: 60,
+    lateralQuickness: 50,
+    helpDIQ: 50,
+    passPerception: 70,
+    defensiveConsistency: 86,
+    offensiveRebounding: 50,
+    defensiveRebounding: 50,
+}
+
+// TERTIARY SKILL - Finihsing - Specializes in dunking and layups -- Can be upgraded to Bronze //
+
+const finishingStats = {
+    layup: 50,
+    standingDunk: 50,
+    drivingDunk: 65,
+    postHook:  50,
+    postFade: 50,
+    postControl: 50,
+    drawFoul: 50,
+    hands: 63,
+}
+
+// PHYSICAL STATS - Specializes in movement and oevrall player athleticism 
+
+const physicalStats = {
+
+    speed: 95,
+    acceleration: 95,
+    strength: 50,
+    vertical: 95,
+    stamina: 50,
+    hustle: 50,
+}
+
+
+// END OF STATS SECTION //
+
+
+// BADGES SECTION - Show the badges my player has. 
+
+
+// SHOOTING BADGES // 
+
+const shootingBadges = {
+    hotZoneHunter: "bronze",
+}
+
+
+
+// PLAYMAKING BADGES //
+
+
+const playmakingBadges = {
+    dimer: "bronze",
+    unpluckable: "bronze",
+}
+
+
+// DEFENSIVE BADGES // 
+
+const defenseBadges = {
+    clamps: "bronze",
+}
+
+
+
+// END OF BADGES SECTION // 
+
+
+// TENDENCIES SECTION -- Shows the tendencies and actions of my player. Might need some adjustments in later seasons. 
+
+
+
+console.log(shootingBadges)
+console.log(playmakingBadges)
